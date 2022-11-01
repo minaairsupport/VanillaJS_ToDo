f30d6c468c9e64cee5230a23f09a250c42b560a7
Mina <mina.fawzy@sbdinc.com>
2022-11-01 16:42:09 +0200

    adding todo button to create todo item

---
+++ app.js
@@ -0,0 +1,51 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//Event Listeners
+
+todoButton.addEventListener('click', addToDo);
+
+// Functions
+
+function addToDo(event) {
+  event.preventDefault();
+  // what we trying to acheive here
+  // <div class="todo">
+  //<li> todo text from input</li>
+  // <button> deleted</button>
+  // <button> checked</button>
+  //</div>
+  console.log('event ', event);
+  // ToDo div
+  const todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo');
+  // ToDo li
+  const todoItem = document.createElement('li');
+  todoItem.classList.add('todo-item');
+  todoItem.innerText = todoInput.value;
+
+  // add to the main div
+  todoDiv.appendChild(todoItem);
+
+  // ToDo checked button
+  const checkedButton = document.createElement('button');
+  checkedButton.innerHTML = '<i class="fa-solid fa-check"/>';
+  checkedButton.classList.add('check-btn');
+
+  // add to the main div
+  todoDiv.appendChild(checkedButton);
+
+  // ToDo checked button
+  const trashButton = document.createElement('button');
+  trashButton.classList.add('trash-btn');
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+
+  // add to the main div
+  todoDiv.appendChild(trashButton);
+
+  todoList.appendChild(todoDiv);
+
+  todoInput.value = '';
+}
